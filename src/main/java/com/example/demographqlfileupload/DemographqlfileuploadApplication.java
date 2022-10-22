70ac72417bda4784e5a49d21c3fd7a2cd591539b
keiji.ueno <keiji.ueno@opentone.co.jp>
2022-10-22 11:17:04 +0900

    initial commit

---
+++ src/main/java/com/example/demographqlfileupload/DemographqlfileuploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.demographqlfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographqlfileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographqlfileuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demographqlfileupload/DemographqlfileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demographqlfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemographqlfileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
